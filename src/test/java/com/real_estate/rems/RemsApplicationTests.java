21a263d848327c47bb53d7bb5434237855c00b14
LAPTOP-JVF79JC0\roger <roger88106@gmail.com>
2023-05-15 18:53:08 +0800

    first commit

---
+++ src/test/java/com/real_estate/rems/RemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.real_estate.rems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
