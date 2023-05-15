e58ea2a077b984f35b6fd767f2e1ded4cf05add1
LAPTOP-JVF79JC0\roger <roger88106@gmail.com>
2023-05-15 19:48:02 +0800

    123

--- src/main/java/com/real_estate/rems/service/impl/第一組的Service.java
+++ src/main/java/com/real_estate/rems/service/impl/第一組的Service.java
@@ -5,9 +5,7 @@ public class 第一組的Service {
     public void 組員一(){
 
         int[] intArr = {1,2,3,4,5};
-        for (int i : intArr) {
-            System.out.println(i);
-        }
+        System.out.println(intArr.length);
     }
 
     public void 組員二的方法(){
